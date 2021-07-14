f4674a30b16806e318126151bbfc995193c3c05d
BlackUserSide <nesmserg@gmail.com>
2021-07-14 12:43:41 +0300

    fits section

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,35 +3,89 @@ import { Link } from "react-router-dom";
 import logo from "../../img/logo.png";
 import user from "../../img/user.png";
 import "./header.sass";
+import "../../libs/hamburgers/dist/hamburgers.css";
+import { useState } from "react";
+
 export const Header: React.FC = () => {
+  const [activeMenu, setActiveMenu] = useState<boolean>();
+  const activeHandler = () => {
+    if (activeMenu) {
+      setActiveMenu(false);
+      return;
+    }
+    setActiveMenu(true);
+  };
   return (
-    <header className="main-header">
-      <div className="logo-wrapper">
-        <img src={logo} alt="logo" />
-      </div>
-      <nav className="main-nav">
-        <ul className="nav-bar">
-          <li className="main-link">
-            <Link to="/">Главная</Link>
-          </li>
-          <li className="main-link">
-            <Link to="/products">Продукты</Link>
-          </li>
-          <li className="main-link">
-            <Link to="/leader">Руководитель</Link>
-          </li>
-          <li className="main-link">
-            <div className="main-auth-link">
-              <img src={user} alt="user-ico" />
+    <>
+      <header className="main-header">
+        <div className="logo-wrapper">
+          <img src={logo} alt="logo" />
+        </div>
+        <nav className="main-nav">
+          <ul className="nav-bar">
+            <li className="main-link">
+              <Link to="/">Главная</Link>
+            </li>
+            <li className="main-link">
+              <Link to="/products">Продукты</Link>
+            </li>
+            <li className="main-link">
+              <Link to="/leader">Руководитель</Link>
+            </li>
+            <li className="main-link">
+              <div className="main-auth-link">
+                <img src={user} alt="user-ico" />
+              </div>
+            </li>
+            <li className="main-link">
+              <div className="change-lang-wrapper">
+                <p>RU</p>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <header className="mobile-header">
+        <div className="logo-wrapper">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="mobile-nav">
+          <div
+            className={`hamburger hamburger--3dx ${
+              activeMenu ? "is-active" : ""
+            }`}
+            onClick={activeHandler}
+          >
+            <div className="hamburger-box">
+              <div className="hamburger-inner"></div>
             </div>
-          </li>
-          <li className="main-link">
-            <div className="change-lang-wrapper">
-              <p>RU</p>
-            </div>
-          </li>
-        </ul>
-      </nav>
-    </header>
+          </div>
+          <nav className={`hidden-menu ${activeMenu ? "active-menu" : ""}`}>
+            <ul className="nav-bar">
+              <li className="main-link">
+                <Link to="/">Главная</Link>
+              </li>
+              <li className="main-link">
+                <Link to="/products">Продукты</Link>
+              </li>
+              <li className="main-link">
+                <Link to="/leader">Руководитель</Link>
+              </li>
+              <li className="main-link">
+                <div className="main-auth-link">
+                  <img src={user} alt="user-ico" />
+                  <p>Войти</p>
+                </div>
+              </li>
+              <li className="main-link">
+                <div className="change-lang-wrapper">
+                  <p>RU</p>
+                </div>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
   );
 };
--- src/components/MainComposition/MainComposition.tsx
+++ src/components/MainComposition/MainComposition.tsx
@@ -2,7 +2,38 @@ import React from "react";
 import { Header } from "../Header/Header";
 import downArrow from "../../img/arrow-down.png";
 import "./maincomposition.sass";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useCallback } from "react";
+import { useMemo } from "react";
+import { useRef } from "react";
 export const MainComposition: React.FC = () => {
+  const [isVis, setIsVis] = useState<boolean>(false);
+  console.log(isVis);
+
+  const options = useMemo(() => {
+    return {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0,
+    };
+  }, []);
+  const testCallbackFunc = useCallback((entrys: any) => {
+    const [entry] = entrys;
+    setIsVis(entry.isIntersecting);
+  }, []);
+  const isRef: any = useRef(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(testCallbackFunc, options);
+
+    if (isRef !== null) {
+      let current = isRef.current;
+      if (current) observer.observe(current);
+      return () => {
+        if (current) observer.unobserve(current);
+      };
+    }
+  }, [testCallbackFunc, options]);
   return (
     <div className="main-composition">
       <Header />
