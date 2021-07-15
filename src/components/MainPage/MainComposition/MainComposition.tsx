c32aa60a5ccddcac14c0acfac31b51b364744bb6
BlackUserSide <nesmserg@gmail.com>
2021-07-15 23:03:36 +0300

    fix

--- src/components/MainPage/MainComposition/MainComposition.tsx
+++ src/components/MainPage/MainComposition/MainComposition.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import { Header } from "../../Header/Header";
 import downArrow from "../../../img/arrow-down.png";
 import "./maincomposition.sass";
-//import videoBg from '../../../img/video.mp4';
 
 export const MainComposition: React.FC = () => {
   return (
     <div className="main-composition">
-      
       <Header />
       <div className="main-text-composition">
         <h1 className="h1">Lotus</h1>
